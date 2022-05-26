02a8c6ca2168ce3bef3b312f0ec53746ac0d2e73
AlexGamer420 <75785342+AleexMaartins@users.noreply.github.com>
2022-05-26 15:25:12 +0100

    1.7 finalmente

--- client-server/common_comm.py
+++ client-server/common_comm.py
@@ -1,7 +1,6 @@
 import socket
 import json
 import base64
-
 #
 # Universal function to send a given amount of data to a TCP socket.
 # It returns True or False, depending on the success of 
@@ -15,7 +14,6 @@ def exact_send (dst, data):
         return True
     except OSError:
         return False
-
 #
 # Universal function to receive a given amount of data from a TCP socket.
 # It returns None or data, depending on the success of
@@ -25,14 +23,10 @@ def exact_recv (src, count):
     data = bytearray (0)
     while count != 0:
         new_data = src.recv (count)
-
         if len (new_data) == 0: return None
-
         data += new_data
         count -= len (new_data)
-
     return data
-
 #
 # Universal function to send a dictionary message to a TCP socket.
 # It actually transmits a JSON object, prefixed by its length (in network byte order).
@@ -42,11 +36,10 @@ def exact_recv (src, count):
 # message to the socket.
 #
 def send_dict (dst, msg):
-    # DEBUG print ("Send: %s" % (msg))
-    data = bytes(json.dumps (msg), "utf8")
-    prefixed_data = len (data).to_bytes (4, "big") + data
+    # DEBUG print ('Send: %s' % (msg))
+    data = bytes(json.dumps (msg), 'utf8')
+    prefixed_data = len (data).to_bytes (4, 'big') + data
     return exact_send (dst, prefixed_data)
-
 #
 # Universal function to receive a dictionary message from a TCP socket.
 # It actually receives a JSON object, prefixed by its length (in network byte order).
@@ -54,18 +47,13 @@ def send_dict (dst, msg):
 #
 def recv_dict (src):
     prefix = exact_recv (src, 4)
-
     if prefix == None: return None
-
-    length = int.from_bytes (prefix, "big")
+    length = int.from_bytes (prefix, 'big')
     data = exact_recv (src, length)
-
     if data == None: return None
-
-    msg = json.loads (str(data, "utf8"))
-    # DEBUG print ("Recv: %s" % (msg))
+    msg = json.loads (str(data, 'utf8'))
+    # DEBUG print ('Recv: %s' % (msg))
     return msg
-
 #
 # Universal function to send and receive a dictionary to/from a TCP socket peer.
 # It returns None upon an error.
