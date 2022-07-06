f72c8be3fcd4dd94cf5476b3db8dfbc9c6f7ff02
Alexandre Martins <75785342+AleexMaartins@users.noreply.github.com>
2022-07-06 15:24:36 +0100

    111

--- Site/OLD/novos trabalhos/js/js.js
+++ Site/OLD/novos trabalhos/js/js.js
@@ -2,12 +2,13 @@ var im1;
 var im2;
 
 function updatePhoto(event) {
-
+    alert("1");
 
     var reader = new FileReader();
     reader = iml1(event);
 
 
+    alert("777");
     im1 = event.target.files[0];
     //Libertar recursos da imagem selecionada
 
@@ -15,6 +16,7 @@ function updatePhoto(event) {
 }
 
 function iml1(event) {
+    alert("ola");
     //Criar uma imagem
     var img = new Image();
     img = im1(img);
@@ -25,22 +27,25 @@ function im1(img) {
     canvas = document.getElementById("photo1");
     ctx = canvas.getContext("2d");
     ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, 530, 400);
-
+    alert("1");
 
 }
 
 function updatePhoto2(event) {
+    alert("1");
 
     var reader = new FileReader();
     reader = iml2(event);
 
 
+    alert("777");
     im2 = event.target.files[0];
     //Libertar recursos da imagem selecionada
 
 }
 
 function iml2(event) {
+    alert("ola");
     //Criar uma imagem
     var img = new Image();
     img = im2(img);
@@ -51,6 +56,7 @@ function im2(img) {
     canvas = document.getElementById("photo2");
     ctx = canvas.getContext("2d");
     ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, 530, 400);
+    alert("1");
 
 }
 
@@ -58,16 +64,16 @@ function im2(img) {
 
 
 function sendFile() {
+    alert("3");
     var data = new FormData();
     data.append("i1", im1);
     data.append("i2", im2);
+    alert("3");
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "done");
     xhr.upload.addEventListener("progress", updateProgress, false);
     xhr.send(data);
 }
-
-
 
 
 
